79167ef5726feafde70fbd154d8b522ff6ef9237
Prem Shah <82882435+PremShah0208@users.noreply.github.com>
2022-06-28 16:50:52 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,5 @@
 class Factorial{  
- public static void main(String args[]){  
-  int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
- }  
-}
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
